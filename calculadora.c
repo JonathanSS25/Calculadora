338b021310f1385e60aaa5a5b20615b2cb42dcc4
ODI24 <andrewoliverbatta@gmail.com>
2025-11-27 15:04:05 -0600

    feat: add division

--- calculadora.c
+++ calculadora.c
@@ -22,7 +22,7 @@ void Suma(){
     printf("El resultado es: %.2f", n1 + n2);
 }
 
-void Multiplicacion()
+void Division()
 {
     float n1 = 0.0, n2 = 0.0;
     printf("Escribe el primer numero a multiplicar: ");
